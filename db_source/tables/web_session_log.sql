fab1fc53f3d0e023bd61d546c9b2d428c547f642
aoware <alain@aoware.co.uk>
2023-02-01 22:11:41 +0000

    Adding web_session_log table

---
+++ db_source/tables/web_session_log.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `web_session_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_token` char(32) NOT NULL COMMENT 'Session used by the user during this http call',
+  `user_id` int(11) NOT NULL COMMENT 'the user_id logged to perform the particular action',
+  `ip` varchar(40) NOT NULL COMMENT 'ip address sent by the browser. In ip4 or ip6 format',
+  `endpoint` varchar(255) NOT NULL COMMENT 'This is the endpoint/url receiving the payload',
+  `method` enum('GET','POST') NOT NULL COMMENT 'Http method sent by the browser',
+  `content_type` varchar(32) DEFAULT NULL COMMENT 'Content type sent by the browser',
+  `payload` text DEFAULT NULL COMMENT 'payload content',
+  `http_return_code` int(11) DEFAULT NULL COMMENT 'http return code. If null, pretty likely the response failed',
+  `created_timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `amended_timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `IDX_web_session_log_user_id` (`user_id`),
+  CONSTRAINT `web_session_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci
